d53cc34c5d1cff2da478f3753a70047757fe070a
Sirawit Pattanawaw <68011145@kmitl.ac.th>
2026-01-16 19:27:59 +0700

    test

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,18 @@
-export function getPostalAddress() {}
+import axios from 'axios'
+
+class Addre{
+	id?:number;
+	name?:string;
+	phone?:string;
+	address?:string;
+}
+
+const getPostalAddress = async () => {
+
+await axios.get<Addre>('https://jsonplaceholder.typicode.com/users')
+.then((res) => {
+	console.log(res.data)
+})
+}
+
+getPostalAddress();
